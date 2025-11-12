4ed16bea8e5622a2f5a85c903d05ec64287f42d7
K-F Balázs <150327324+WotBALU@users.noreply.github.com>
2025-11-12 09:00:30 +0100

    Add files via upload

---
+++ FogadoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Fogado.Models;
+
+
+namespace Fogado.Data
+{
+    public class FogadoContext : DbContext
+    {
+        public FogadoContext(DbContextOptions<FogadoContext> options) : base(options) { }
+        public DbSet<Szoba> Szobak {  get; set; }
+        public DbSet<Vendeg> Vendegek { get; set; }
+        public DbSet<Foglalas> Foglalasok { get; set; }
+    }
+}
